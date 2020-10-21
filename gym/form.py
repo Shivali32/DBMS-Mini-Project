8840d7d4cfef59316fe5ef491ff6654c8ac9c75f
Shivali32 <shivaliparopkare@gmail.com>
2020-10-21 22:52:36 +0530

    Search Basic

--- gym/form.py
+++ gym/form.py
@@ -4,9 +4,9 @@ from gym.models import member, trainer, facility
 class memberForm(forms.ModelForm):  
     class Meta:  
         model = member
-        fields = ("m_name", "m_age", "m_phno","m_time","m_years")
+        fields = ("__all__")
 
 class trainerForm(forms.ModelForm):  
     class Meta:  
         model = trainer
-        fields = ("t_name", "t_age", "t_phno","t_hours","t_salary")
+        fields = ("__all__")
--- gym/views.py
+++ gym/views.py
@@ -46,27 +46,179 @@ def EnterMember(request):
 
 
 def showTrainer(request):
-    df = pd.DataFrame(columns = ['Name', 'Age','Number','Salary']) 
     
-    for t in trainer.objects.raw('SELECT * FROM trainer;'):
-        df = df.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Salary': t.t_salary}, ignore_index=True)
-        
-    
-    print(df)
-    plotlyDict=showtableTrainer(df)
-    return render(request,"showTrainer.html",plotlyDict)
+    df = dfn = dfa_g = dfa_l = dfp = dfh_g = dfh_l = dfs_g = dfs_l = pd.DataFrame(columns = ['Name', 'Age','Number','Hours','Salary']) 
+    #df.drop(columns = ['Name', 'Age','Number','Hours','Salary'])
 
+    if request.method == 'GET':
+        for t in trainer.objects.raw('SELECT * FROM trainer;'):
+            df = df.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)            
+        print(df)
+        plotlyDict=showtableTrainer(df)
+        return render(request,"showTrainer.html",plotlyDict)
+    elif request.method == 'POST':
+        name  = request.POST['name']
+        age_g  = request.POST['age_g']
+        age_l  = request.POST['age_l']
+        phno  = request.POST['phno']
+        hours_g  = request.POST['hours_g']
+        hours_l  = request.POST['hours_l']
+        salary_g  = request.POST['salary_g']
+        salary_l  = request.POST['salary_l']        
+        trainers = [name,age_g,age_l,phno,hours_g,hours_l,salary_g,salary_l]
+        print(trainers)
+        
+        if name:
+            for t in trainer.objects.raw('SELECT * FROM trainer WHERE t_name == %s ;', [name]):
+                dfn = dfn.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)            
+            print(dfn)
+        else:
+            for t in trainer.objects.raw('SELECT * FROM trainer;'):
+                dfn = dfn.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)                      
+        if age_g:
+            for t in trainer.objects.raw('SELECT * FROM trainer WHERE t_age >= %s ;', [age_g]):
+                dfa_g = dfa_g.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)            
+            print(dfa_g)
+        else:
+            for t in trainer.objects.raw('SELECT * FROM trainer;'):
+                dfa_g = dfa_g.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)                      
+        dfm = pd.merge(dfn, dfa_g, how ='inner')
+        if age_l:
+            for t in trainer.objects.raw('SELECT * FROM trainer WHERE t_age <= %s ;', [age_l]):
+                dfa_l = dfa_l.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)            
+            print(dfa_l)
+        else:
+            for t in trainer.objects.raw('SELECT * FROM trainer;'):
+                dfa_l = dfa_l.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)                      
+        dfm = pd.merge(dfm, dfa_l, how ='inner')
+        if phno:
+            for t in trainer.objects.raw('SELECT * FROM trainer WHERE t_phno == %s ;', [phno]):
+                dfp = dfp.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)            
+            print(dfp)
+        else:
+            for t in trainer.objects.raw('SELECT * FROM trainer;'):
+                dfp = dfp.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)                      
+        dfm = pd.merge(dfm, dfp, how ='inner')
+        if hours_g:
+            for t in trainer.objects.raw('SELECT * FROM trainer WHERE t_hours >= %s ;', [hours_g]):
+                dfh_g = dfh_g.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)            
+            print(dfh_g)
+        else:
+            for t in trainer.objects.raw('SELECT * FROM trainer;'):
+                dfh_g = dfh_g.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)                      
+        dfm = pd.merge(dfm, dfh_g, how ='inner')
+        if hours_l:
+            for t in trainer.objects.raw('SELECT * FROM trainer WHERE t_hours <= %s ;', [hours_l]):
+                dfh_l = dfh_l.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)            
+            print(dfh_l)
+        else:
+            for t in trainer.objects.raw('SELECT * FROM trainer;'):
+                dfh_l = dfh_l.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)                      
+        dfm = pd.merge(dfm, dfh_l, how ='inner')
+        if salary_g:
+            for t in trainer.objects.raw('SELECT * FROM trainer WHERE t_salary >= %s ;', [salary_g]):
+                dfs_g = dfs_g.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)            
+            print(dfs_g)
+        else:
+            for t in trainer.objects.raw('SELECT * FROM trainer;'):
+                dfs_g = dfs_g.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)                      
+        dfm = pd.merge(dfm, dfs_g, how ='inner')
+        if salary_l:
+            for t in trainer.objects.raw('SELECT * FROM trainer WHERE t_salary <= %s ;', [salary_l]):
+                dfs_l = dfs_l.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)            
+            print(dfs_l)
+        else:
+            for t in trainer.objects.raw('SELECT * FROM trainer;'):
+                dfs_l = dfs_l.append({'Name':t.t_name ,'Age': t.t_age,'Number':t.t_phno,'Hours':t.t_hours,'Salary': t.t_salary}, ignore_index=True)                      
+        dfm = pd.merge(dfm, dfs_l, how ='inner')
+                
+        
+        print('dfm' + dfm) 
+        plotlyDict=showtableTrainer(dfm)
+        return render(request,"showTrainer.html",plotlyDict)
 
 
 def showMember(request):
-    df = pd.DataFrame(columns = ['Name', 'Age','Number']) 
-    for m in member.objects.raw('SELECT * FROM member;'):
-        df = df.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno}, ignore_index=True)
-       
-    #df=getDataframeMember(membersAge,membersName,membersNumber)
-    print (df)
-    plotlyDict=showtableMember(df)
-    return render(request,"showMember.html",plotlyDict)
+    df = dfn = dfa_g = dfa_l = dfp = dft = dfy_g = dfy_l = pd.DataFrame(columns = ['Name', 'Age','Number','Time','Years']) 
+    
+    if request.method == 'GET':
+        for m in member.objects.raw('SELECT * FROM member;'):
+            df = df.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno,'Time': m.m_time,'Years':m.m_years}, ignore_index=True)        
+        #df=getDataframeMember(membersAge,membersName,membersNumber)
+        print (df)
+        plotlyDict=showtableMember(df)
+        return render(request,"showMember.html",plotlyDict)
+    elif request.method == 'POST':
+        name  = request.POST['name']
+        age_g  = request.POST['age_g']
+        age_l  = request.POST['age_l']
+        phno  = request.POST['phno']
+        time  = request.POST['time']
+        years_g  = request.POST['years_g']
+        years_l  = request.POST['years_l']        
+        members = [name,age_g,age_l,phno,time,years_g,years_l]
+        print(members)
+        
+        if name:
+            for m in member.objects.raw('SELECT * FROM member WHERE m_name == %s ;', [name]):
+                dfn = dfn.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno,'Time': m.m_time,'Years':m.m_years}, ignore_index=True)        
+            print(dfn)
+        else:
+            for m in member.objects.raw('SELECT * FROM member;'):
+                dfn = dfn.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno,'Time': m.m_time,'Years':m.m_years}, ignore_index=True)        
+        if age_g:
+            for m in member.objects.raw('SELECT * FROM member WHERE m_age >= %s ;', [age_g]):
+                dfa_g = dfa_g.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno,'Time': m.m_time,'Years':m.m_years}, ignore_index=True)        
+            print(dfa_g)
+        else:
+            for m in member.objects.raw('SELECT * FROM member;'):
+                dfa_g = dfa_g.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno,'Time': m.m_time,'Years':m.m_years}, ignore_index=True)        
+        dfm = pd.merge(dfn, dfa_g, how ='inner')
+        if age_l:
+            for m in member.objects.raw('SELECT * FROM member WHERE m_age <= %s ;', [age_l]):
+                dfa_l = dfa_l.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno,'Time': m.m_time,'Years':m.m_years}, ignore_index=True)        
+            print(dfa_l)
+        else:
+            for m in member.objects.raw('SELECT * FROM member;'):
+                dfa_l = dfa_l.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno,'Time': m.m_time,'Years':m.m_years}, ignore_index=True)        
+        dfm = pd.merge(dfm, dfa_l, how ='inner')
+        if phno:
+            for m in member.objects.raw('SELECT * FROM member WHERE m_phno == %s ;', [phno]):
+                dfp = dfp.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno,'Time': m.m_time,'Years':m.m_years}, ignore_index=True)        
+            print(dfp)
+        else:
+            for m in member.objects.raw('SELECT * FROM member;'):
+                dfp = dfp.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno,'Time': m.m_time,'Years':m.m_years}, ignore_index=True)        
+        dfm = pd.merge(dfm, dfp, how ='inner')
+        if time:
+            for m in member.objects.raw('SELECT * FROM member WHERE m_time >= %s ;', [time]):
+                dft = dft.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno,'Time': m.m_time,'Years':m.m_years}, ignore_index=True)        
+            print(dft)
+        else:
+            for m in member.objects.raw('SELECT * FROM member;'):
+                dft = dft.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno,'Time': m.m_time,'Years':m.m_years}, ignore_index=True)        
+        dfm = pd.merge(dfm, dft, how ='inner')
+        if years_g:
+            for m in member.objects.raw('SELECT * FROM member WHERE m_years >= %s ;', [years_g]):
+                dfy_g = dfy_g.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno,'Time': m.m_time,'Years':m.m_years}, ignore_index=True)        
+            print(dfy_g)
+        else:
+            for m in member.objects.raw('SELECT * FROM member;'):
+                dfy_g = dfy_g.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno,'Time': m.m_time,'Years':m.m_years}, ignore_index=True)        
+        dfm = pd.merge(dfm, dfy_g, how ='inner')
+        if years_l:
+            for m in member.objects.raw('SELECT * FROM member WHERE m_years <= %s ;', [years_l]):
+                dfy_l = dfy_l.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno,'Time': m.m_time,'Years':m.m_years}, ignore_index=True)        
+            print(dfy_l)
+        else:
+            for m in member.objects.raw('SELECT * FROM member;'):
+                dfy_l = dfy_l.append({'Name':m.m_name ,'Age': m.m_age,'Number':m.m_phno,'Time': m.m_time,'Years':m.m_years}, ignore_index=True)        
+        dfm = pd.merge(dfm, dfy_l, how ='inner')
+                
+        
+        print('dfm' + dfm) 
+        plotlyDict=showtableMember(dfm)
+        return render(request,"showMember.html",plotlyDict)
 
 
 
@@ -125,10 +277,10 @@ def deleteTrainer(request):
 
 def showtableMember(df):
     fig = go.Figure(data=[go.Table(
-    header=dict(values=list(["Name", "Age","Number"]),
+    header=dict(values=list(["Name", "Age","Number","Time","Years"]),
                 fill_color='paleturquoise',
                 align='left'),
-    cells=dict(values=[df['Name'], df['Age'], df['Number']],
+    cells=dict(values=[df['Name'], df['Age'], df['Number'],df['Time'],df['Years']],
                fill_color='lavender',
                align='left'))
     ])
@@ -137,23 +289,25 @@ def showtableMember(df):
     return plotDict
 
 
-def getDataframeMember(membersAge,membersName,membersNumber):
-    df = pd.DataFrame(columns = ['Name', 'Age','Number']) 
-    for (a,b,c) in zip(membersAge,membersName,membersNumber):
+def getDataframeMember(membersAge,membersName,membersNumber,membersTime,membersYears):
+    df = pd.DataFrame(columns = ['Name', 'Age','Number','Time','Years']) 
+    for (a,b,c,d,e) in zip(membersAge,membersName,membersNumber,membersTime,membersYears):
         df = df.append({'Name': b}, ignore_index=True)
         df = df.append({'Age': a}, ignore_index=True)
         df = df.append({'Number': c}, ignore_index=True)
-
+        df = df.append({'Time': d}, ignore_index=True)
+        df = df.append({'Years': e}, ignore_index=True)
     return df
 
 
-def getDataframeTrainer(trainersAge,trainersName,trainersNumber,trainersSalary):
-    df = pd.DataFrame(columns = ['Name', 'Age','Number','Salary']) 
-    for (a,b,c,d) in zip(trainersAge,trainersName,trainersNumber,trainersSalary):
+def getDataframeTrainer(trainersAge,trainersName,trainersNumber,trainerHours,trainersSalary):
+    df = pd.DataFrame(columns = ['Name', 'Age','Number','Hours','Salary']) 
+    for (a,b,c,d,e) in zip(trainersAge,trainersName,trainersNumber,trainerHours,trainersSalary):
         df = df.append({'Name': b}, ignore_index=True)
         df = df.append({'Age': a}, ignore_index=True)
         df = df.append({'Number': c}, ignore_index=True)
-        df = df.append({'Salary': d}, ignore_index=True)
+        df = df.append({'Hours': d}, ignore_index=True)
+        df = df.append({'Salary': e}, ignore_index=True)
     
 
     return df
@@ -161,10 +315,10 @@ def getDataframeTrainer(trainersAge,trainersName,trainersNumber,trainersSalary):
 
 def showtableTrainer(df):
     fig = go.Figure(data=[go.Table(
-    header=dict(values=list(["Name", "Age","Number","Salary"]),
+    header=dict(values=list(["Name", "Age","Number","Hours","Salary"]),
                 fill_color='paleturquoise',
                 align='left'),
-    cells=dict(values=[df['Name'], df['Age'], df['Number'],df['Salary']],
+    cells=dict(values=[df['Name'], df['Age'], df['Number'],df['Hours'],df['Salary']],
                fill_color='lavender',
                align='left'))
     ])
