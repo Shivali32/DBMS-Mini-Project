272f4487de4b3d48a98867c1fd5d7a97d6fbd993
Shivali32 <shivaliparopkare@gmail.com>
2020-10-27 10:58:06 +0530

    Minor Frontend Changes

--- gym/views.py
+++ gym/views.py
@@ -390,14 +390,14 @@ def updateTrainer(request):
         name = request.POST['name']
         print(name)
         for t in trainer.objects.raw('SELECT * FROM trainer WHERE t_name == %s ;', [name]):
-            t = {   't_name' : t.t_name, 
+            trn = {   't_name' : t.t_name, 
                     't_age' : t.t_age,
                     't_phno' : t.t_phno,
                     't_hours' : t.t_hours,
                     't_salary' : t.t_salary,
                     't_fid' : t.t_fid
                 }      
-        print(t)
+        print(trn)
 
         if 'update' in request.POST:
             print('2-----------------------------------------------------------------------------------------------------')
@@ -413,8 +413,9 @@ def updateTrainer(request):
             cursor = connection.cursor()
             #cursor.execute("UPDATE trainer SET t_name = %s, t_age = %s, t_phno = %s, t_hours = %s, t_salary = %s WHERE t_name == %s ;", [namei, age, phno, hours, salary, name])
             cursor.execute("UPDATE trainer SET t_age = %s, t_phno = %s, t_hours = %s, t_salary = %s WHERE t_name == %s ;", [age, phno, hours, salary, name])
+            return HttpResponseRedirect('/') 
 
-        return render(request, "updateTrainer.html", t)
+        return render(request, "updateTrainer.html", trn)
 
 def updateMember(request):
 
@@ -427,14 +428,14 @@ def updateMember(request):
         name = request.POST['name']
         print(name)
         for m in member.objects.raw('SELECT * FROM member WHERE m_name == %s ;', [name]):
-            m = {   'm_name' : m.m_name, 
+            mem = {   'm_name' : m.m_name, 
                     'm_age' :  m.m_age,
                     'm_phno' : m.m_phno,
                     'm_time' : m.m_time,
                     'm_years' : m.m_years,
                     'm_fid' : m.m_fid
                 }      
-        print(m)
+        print(mem)
 
         if 'update' in request.POST:
             print('2-----------------------------------------------------------------------------------------------------')
@@ -448,5 +449,6 @@ def updateMember(request):
             cursor = connection.cursor()
             #cursor.execute("UPDATE member SET m_name = %s, m_age = %s, m_phno = %s, m_hours = %s, m_salary = %s WHERE m_name == %s ;", [namei, age, phno, time, years, name])
             cursor.execute("UPDATE member SET m_age = %s, m_phno = %s, m_time = %s, m_years = %s WHERE m_name == %s ;", [age, phno, time, years, name])
+            return HttpResponseRedirect('/') 
 
-        return render(request, "updateMember.html", m)
+        return render(request, "updateMember.html", mem)
